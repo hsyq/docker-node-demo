9f357957dbf570ec990d86a0ea7e7b89937324ed
NikitaCP <lijianzly@gmail.com>
2017-12-09 15:20:37 +0800

    用完即废

--- app.js
+++ app.js
@@ -9,4 +9,19 @@ http.createServer(function(request,response){
 	response.write("Hello,NodeJS!"); 
 	response.end(); 
 	
-}).listen(8080,"localhost");
+}).listen(8888,"localhost");
+
+const http = require('http');
+
+http.createServer(function(request,response){
+	
+	response.statusCode = 200;  
+	response.setHeader("Content-type","text/html;charset=utf-8");
+	response.setHeader('name','nodejs');
+	
+	response.write("Hello,NodeJS!"); 
+	response.end(); 
+	
+}).listen(80,"172.17.199.10:4");
+console.log("Server is running");
+
